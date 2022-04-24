246edbc38f3ebc57954cb0ab7503f4e488e852e1
IFERNAN1 <isac.fernandes@volvocars.com>
2022-04-24 15:08:44 +0200

    added subbytes

--- AESAlgorithm/AESAlgorithm/AESAlgorithm.cpp
+++ AESAlgorithm/AESAlgorithm/AESAlgorithm.cpp
@@ -1,12 +1,30 @@
 #include"AESalgorithm.h"
 
 
-unsigned char AES::AddRoundKey(unsigned char plaintext[], unsigned char key[]) {
+std::vector<unsigned char> AES::SubBytes(std::string plaintext, std::string key)
+{
+	 auto xorcalc = [=]()->void { for (int i = 0; i < 15; i++) { plaintextXORkey.push_back((int)plaintext[i] ^ key[i]);} };
 
-	for (int i = 0; i < 15; i++)
-	{
-		AES::round_key_output.push_back(plaintext[i] ^ key[i]);
 
-	}
-	return 0;
+	 std::vector<uint8_t> map_sbox;
+	 uint8_t column;
+	 uint8_t line;
+
+	 for (auto itr : plaintextXORkey) {
+
+		 column = itr & 0b00001111;
+		 line   = itr & 0b11110000;
+		 map_sbox.push_back(sBox[line][column]);
+	 }
+
+	return  std::vector<unsigned char>();
+}
+
+std::vector<unsigned char> AES::Encrypt(std::string to_encrypt, std::string key)
+{
+	std::vector<unsigned char> AddRoundKey;
+
+	AddRoundKey = AES::SubBytes(to_encrypt, key);
+
+	return std::vector<unsigned char>();
 }
--- AESAlgorithm/AESAlgorithm/main.cpp
+++ AESAlgorithm/AESAlgorithm/main.cpp
@@ -1,8 +1,39 @@
 #include"AESalgorithm.h"
+#include<memory>
+
+#include <bitset>
 
 void main()
 {
-	int to_send[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+	std::string to_encryp = "isac";
+	std::vector<uint8_t> test;
+	int b = (int)to_encryp.at(1);
+	 uint8_t c = 116;
+	//test.push_back((int)to_encryp.at(1));
+
+	std::vector<unsigned char>        key{ 0xB,0xC,13,12,11,10,15,14,13,12,11,10,9,8,7,6 };
+
+	//std::vector<std::vector<uint8_t> > fog{ 
+	//	{ 0x63,0x7c,0x77,0x7b },
+	//	{ 0xca,0x82,0xc9,0x7d} 
+	//};
+
+	//for(auto i: test)
+	//std::cout << b << std::endl;
+
+	//c = (uint8_t) b;
+	//std::cout << unsigned(c) << std::endl;
+	//std::bitset<8> x(c&0b00001111);
+	//std::cout << x << std::endl;
+
+	//std::cout << c << std::endl;
+
+
+	std::vector<unsigned char> encrypted;
+
+	std::unique_ptr<AES> message;
+
+	//encrypted = message->Encrypt(to_encrypt, key);
 	
 
 }
