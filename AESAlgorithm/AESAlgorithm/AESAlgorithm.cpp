ac644f0e12decfb16ddd2c39f7fcfc43b7f2a888
IFERNAN1 <isac.fernandes@volvocars.com>
2022-05-01 22:30:37 +0200

    Sub-Bytes

--- AESAlgorithm/AESAlgorithm/AESAlgorithm.cpp
+++ AESAlgorithm/AESAlgorithm/AESAlgorithm.cpp
@@ -3,28 +3,118 @@
 
 std::vector<unsigned char> AES::SubBytes(std::string plaintext, std::string key)
 {
-	 auto xorcalc = [=]()->void { for (int i = 0; i < 15; i++) { plaintextXORkey.push_back((int)plaintext[i] ^ key[i]);} };
+
+	////Xor add RoundKey
+	//auto xorcalc = [=]()->void { for (int i = 0; i < 15; i++) { plaintextXORkey.push_back((int)plaintext[i] ^ key[i]);} };
 
 
-	 std::vector<uint8_t> map_sbox;
-	 uint8_t column;
-	 uint8_t line;
+	////SubBytes
+	// std::vector<uint8_t> map_sbox;
+	// uint8_t column;
+	// uint8_t line;
 
-	 for (auto itr : plaintextXORkey) {
+	// for (auto itr : plaintextXORkey) {
 
-		 column = itr & 0b00001111;
-		 line   = itr & 0b11110000;
-		 map_sbox.push_back(sBox[line][column]);
-	 }
+	//	 column = itr & 0b00001111;
+	//	 line   = itr & 0b11110000;
+	//	 map_sbox.push_back(sBox[line][column]);
+	// }
+
+	// //Shift Rows
+
 
 	return  std::vector<unsigned char>();
 }
 
+char buffer[20];
+std::string str("Test string...");
+std::size_t length = str.copy(buffer, 6, 5);
+
 std::vector<unsigned char> AES::Encrypt(std::string to_encrypt, std::string key)
 {
-	std::vector<unsigned char> AddRoundKey;
+	// pack creation 
+	char  pack[15], keyL[15];
+	to_encrypt.copy(pack, 15, 0);
+	key.copy(keyL, 15, 0);
 
-	AddRoundKey = AES::SubBytes(to_encrypt, key);
+	//std::cout << to_encrypt << std::endl;
+	//for (int j = 0; j < 15; j++) {
+	//	std::cout << pack[j] ;
+	//}
+	//std::cout<<   "\n";
+	//
+	//for (int j = 0; j < 15; j++) {
+	//	std::cout << keyL[j] ;
+	//}
+
+	//std::cout << "\n";
+
+	//Xor add RoundKey
+	char XorAddRoundKey[15];
+	auto xorcalc = [&]()->void { for (int i = 0; i < 15; i++) { XorAddRoundKey[i] = pack[i] ^ keyL[i]; } };
+	xorcalc();
+
+	//Sub-Bytes:
+	char mapSbox[15];
+
+	for (int i = 0; i < 15; i++) {
+
+		std::bitset<8> RowColumn(XorAddRoundKey[i]);
+		//row , column
+		mapSbox[i] = sBox[postsBox[((RowColumn.to_ulong() & 0b11110000) >> 4)][(RowColumn.to_ulong() & 0b00001111)]];
+
+	}
+	//std::bitset<8> RowColumn(XorAddRoundKey[0]);
+	//std::cout << XorAddRoundKey[2] << '\n';
+	//std::cout << RowColumn << std::endl;
+
+	//uint8_t t = RowColumn.to_ulong();
+	//uint8_t column = (( RowColumn.to_ulong()&0b11110000 ) >> 4 );
+	//uint8_t row = ( RowColumn.to_ulong() & 0b00001111 );
+	//printf("\n%d", column);
+	//printf("\n%d", row);
+
+
+
+
+	//uint row = (4 >> RowColumn);
+	//uint8_t column = (RowColumn << 4);
+
+	 //std::cout << (x & 0b00001111)<< '\n';
+
+		//	 column = itr & 0b00001111;
+		 //line   = itr & 0b11110000;
+
+
+	/*for (int j = 0; j < 15; j++) {
+		std::cout << XorAddRoundKey[j] << std::endl;
+	}*/
+
+	//auto xorcalc = [=]()->void { for (int i = 0; i < 15; i++) { plaintextXORkey.push_back((int)to_encrypt[i] ^ key[i]); } };
+
+	//std::cout << plaintextXORkey << std::endl;
+
+	//std::vector<std::vector<std::string>> plaintext;
+
+	//uint8_t flow_to_encrypt = 0;
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	for(int j = 0; j <2; j++)
+	//	{
+	//		
+	//		flow_to_encrypt++;
+	//	
+	//	}
+	//}
+	//const char* str = to_encrypt.c_str();
+
+
+	//std::cout << str << std::endl;
+
+	//plaintext[1][1] = "1";
+	//std::vector<unsigned char> AddRoundKey;
+
+	//AddRoundKey = AES::SubBytes(to_encrypt, key);
 
 	return std::vector<unsigned char>();
 }
--- AESAlgorithm/AESAlgorithm/main.cpp
+++ AESAlgorithm/AESAlgorithm/main.cpp
@@ -1,17 +1,17 @@
 #include"AESalgorithm.h"
-#include<memory>
 
-#include <bitset>
 
 void main()
 {
-	std::string to_encryp = "isac";
+	std::string to_encryp = "isac Newton Artuso Fernandes";
+	std::string key = "rtyuioplkjhgtre";
+
 	std::vector<uint8_t> test;
 	int b = (int)to_encryp.at(1);
 	 uint8_t c = 116;
 	//test.push_back((int)to_encryp.at(1));
 
-	std::vector<unsigned char>        key{ 0xB,0xC,13,12,11,10,15,14,13,12,11,10,9,8,7,6 };
+	//std::vector<unsigned char> key{ 0xB,0xC,13,12,11,10,15,14,13,12,11,10,9,8,7,6 };
 
 	//std::vector<std::vector<uint8_t> > fog{ 
 	//	{ 0x63,0x7c,0x77,0x7b },
@@ -29,11 +29,37 @@ void main()
 	//std::cout << c << std::endl;
 
 
+	 char ch1 = 'I', ch2 = 'S', ch3;
+	 ch3 = ch1 ^ ch2;
+	 std::bitset<8> x(ch1);
+	// std::cout << x << '\n';;
+	 //std::cout << (x & 0b00001111)<< '\n';
+
+	 	//	 column = itr & 0b00001111;
+		 //line   = itr & 0b11110000;
+
+	 //printf("char %d\n", ch3);
+	 //printf("char move 4 bits %d\n", ch3<<4);
+	 //printf("%c\n", ch1 - ch2 - 4);
+
+
+	 std::string s1, s2, s3;
+
+	 s1 = 'I';
+	 s2 = 'S';
+
+	 const char* c1 = s1.c_str();
+	 const char* c2 = s2.c_str();
+
+	 //printf("String to char %d\n", *c1^*c2);
+
+
+
 	std::vector<unsigned char> encrypted;
 
 	std::unique_ptr<AES> message;
 
-	//encrypted = message->Encrypt(to_encrypt, key);
+	encrypted = message->Encrypt(to_encryp, key);
 	
 
 }
